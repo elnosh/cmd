c437079cfd7f3645ad7466cf27656c3e502bd8a5
miguelhun <hunmiguel1124@gmail.com>
2020-08-18 11:19:14 -0500

    make new directories

--- cmd/make.go
+++ cmd/make.go
@@ -11,31 +11,36 @@ var file string
 var directory string
 
 var createCmd = &cobra.Command{
-	Use: "create",
+	Use: "mk",
 	Short: "Make a new file or directory",
 	Run: func(cmd *cobra.Command, args []string) {
 
 		if file != "" {
-			fmt.Println("file name: ", file)
-
 			f, err := os.Create(file)
 			if err != nil {
-				log.Error("There was an error while creating file")
+				log.Error("There was an error creating file.")
 			}
 			defer f.Close()
 		}
 
+		if directory != "" {
+			err := os.Mkdir(directory, 0755)
+			if err != nil {
+				log.Error("There was an error creating the directory")
+			}
+			//defer os.RemoveAll(directory)
+		}
+
 		//fmt.Println("create: " + strings.Join(args, " "))
-		//fmt.Println("directory name: ", directory)
+		fmt.Println("file name: ", file)
+		fmt.Println("directory name: ", directory)
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(createCmd)
 
-
 	createCmd.Flags().StringVarP(&file, "file", "f", "", "set file name")
 	createCmd.Flags().StringVarP(&directory, "directory", "d", "", "set directory name")
-
 }
 
