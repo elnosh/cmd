48b3e40988a7671c2ddfcdb53daae6b3a9d0d985
miguelhun <hunmiguel1124@gmail.com>
2020-08-20 11:55:30 -0500

    change for creating new directory

--- cmd/mkdir.go
+++ cmd/mkdir.go
@@ -2,17 +2,27 @@ package cmd
 
 import (
 	"fmt"
-
 	"github.com/spf13/cobra"
+	"os"
 )
 
-// mkdirCmd represents the mkdir command
 var mkdirCmd = &cobra.Command{
 	Use:   "mkdir",
 	Short: "Make a new directory",
 
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("mkdir called")
+
+		for _, arg := range args {
+			if _, err := os.Stat(arg); !os.IsNotExist(err) {
+				fmt.Println("File or directory already exists.")
+			} else {
+				err := os.Mkdir(arg, 0755)
+				if err != nil {
+					fmt.Println("There was an error creating the directory.")
+				}
+			}
+		}
+
 	},
 }
 
